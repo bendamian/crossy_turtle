a55d28785386ed5905e73b6780053c7e865fe22f
bendamian <badamian@gmail.com>
2023-12-04 16:22:33 +0000

    move the turtle with keypress

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player()
+
+
+screen.listen()
+screen.onkey(player.go_up, 'Up')
+screen.onkey(player.go_down,"Down")
+screen.onkey(player.go_left,"Left")
+screen.onkey(player.go_right,"Right")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -1,7 +1,29 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.color('green')
+        self.setheading(90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_down(self):
+        self.back(MOVE_DISTANCE)
+
+    def go_left(self):
+        new_x = self.xcor() - MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def go_right(self):
+        new_x = self.xcor() + MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
