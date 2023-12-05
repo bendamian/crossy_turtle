4201accc51f7c7f71f174a53fa40b023ef4e6065
bendamian <badamian@gmail.com>
2023-12-05 11:38:24 +0000

    add level to game

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, 'Up')
@@ -26,9 +27,10 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
     # Detect a successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
-
+        scoreboard.increase_level()
 screen.exitonclick()
